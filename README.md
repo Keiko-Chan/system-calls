b18e97337be39ecc7f039b677a4e998ecbd94c9a
Ekaterina <dudenko.ei@phystech.edu>
2021-09-15 23:00:50 +0300

    some changes

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # system-calls
-2 Second course
+# Second course
+
+Stat - вносит в структуру, на которую указывает statbuf, информацию, содержащуюся в(o) файле (его надо открыть), связанном с указателем filename. Структура stat определена в sys\stat.h. 
++++int stat (const char *pathname, struct stat *buf);
+
+Lstat - похожа на stat, но возвращает информацию о символической ссылке, а не о файле(файл открывать не надо) на который та указывает. 
++++int lstat(const char *pathname, struct stat *buf);
+
+Statx - Эта функция возвращает информацию о файле, сохраняя ее в буфер, на который указывает statxbuf. 
++++int statx(int dirfd, const char *pathname, int flags, unsigned int mask, struct statx *statxbuf);
+
--- stat.c
+++ stat.c
@@ -4,12 +4,48 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/sysmacros.h>
-//#include<iostream>
+
+char* get_file_type(const struct stat *sb)
+{
+	char* type;
+	switch (sb->st_mode & S_IFMT)
+        {
+         	case S_IFBLK:  type = "block device";            break;
+         	case S_IFCHR:  type = "character device";        break;
+         	case S_IFDIR:  type = "directory";               break;
+         	case S_IFIFO:  type = "FIFO/pipe";               break;
+         	case S_IFLNK:  type = "symlink";                 break;
+         	case S_IFREG:  type = "regular file";            break;
+         	case S_IFSOCK: type = "socket";                  break;
+         	default:       type = "unknown?";                break;
+        }
+        
+        return type;
+}
+
+void print_access(const struct stat *sb)
+{
+	printf("access:                   ");
+	printf( (S_ISDIR(sb->st_mode)) ? "d" : "-");
+	printf( (sb->st_mode & S_IRUSR) ? "r" : "-");
+    	printf( (sb->st_mode & S_IWUSR) ? "w" : "-");
+   	printf( (sb->st_mode & S_IXUSR) ? "x" : "-");
+   	printf( (sb->st_mode & S_IRGRP) ? "r" : "-");
+   	printf( (sb->st_mode & S_IWGRP) ? "w" : "-");
+  	printf( (sb->st_mode & S_IXGRP) ? "x" : "-");
+  	printf( (sb->st_mode & S_IROTH) ? "r" : "-");
+	printf( (sb->st_mode & S_IWOTH) ? "w" : "-");
+    	printf( (sb->st_mode & S_IXOTH) ? "x" : "-");
+    	printf("\n");
+
+}
 
 int main(int argc, char *argv[])
 {
 	struct stat sb;
 	char s[26];
+	char *type;
+	struct tm bdt;
 	
 	if (argc != 2)
 	{
@@ -23,23 +59,13 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 	
+	type = get_file_type(&sb);
+	
 	printf("ID of containing device:  [%lx,%lx]\n", (long) major(sb.st_dev), (long) minor(sb.st_dev));
 
-        printf("File type:                ");
-
-        switch (sb.st_mode & S_IFMT)
-        {
-         	case S_IFBLK:  printf("block device\n");            break;
-         	case S_IFCHR:  printf("character device\n");        break;
-         	case S_IFDIR:  printf("directory\n");               break;
-         	case S_IFIFO:  printf("FIFO/pipe\n");               break;
-         	case S_IFLNK:  printf("symlink\n");                 break;
-         	case S_IFREG:  printf("regular file\n");            break;
-         	case S_IFSOCK: printf("socket\n");                  break;
-         	default:       printf("unknown?\n");                break;
-        }
-
+        printf("File type:                %s\n", type); 
         printf("I-node number:            %ld\n", (long) sb.st_ino);
+        print_access(&sb);
         printf("Mode:                     %lo (octal)\n", (unsigned long) sb.st_mode);
         printf("Link count:               %ld\n", (long) sb.st_nlink);
         printf("Ownership:                UID=%ld   GID=%ld\n", (long) sb.st_uid, (long) sb.st_gid);
@@ -49,12 +75,17 @@ int main(int argc, char *argv[])
         printf("Blocks allocated:         %lld\n", (long long) sb.st_blocks);
 
 	ctime_r(&sb.st_ctime, s);
-        printf("\nLast status change:       %s", s);
+	localtime_r (&sb.st_ctime, &bdt);
+        printf("Last status change:       %s %s", bdt.tm_zone, s);
+        
         ctime_r(&sb.st_atime, s);
-        printf("Last file access:         %s", s);
+        localtime_r (&sb.st_atime, &bdt);
+        printf("Last file access:         %s %s", bdt.tm_zone, s);
+        
         ctime_r(&sb.st_mtime, s);
-        printf("Last file modification:   %s", s);
-
+        localtime_r (&sb.st_mtime, &bdt);
+        printf("Last file modification:   %s %s", bdt.tm_zone, s);
+        
         exit(EXIT_SUCCESS);
 }
 
