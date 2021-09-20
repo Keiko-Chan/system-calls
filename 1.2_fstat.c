ed059957cb39e9e0ee48796d930ad0f790c11bdb
Ekaterina <dudenko.ei@phystech.edu>
2021-09-20 15:02:08 +0300

    add fstat, dprintf, modifyed stat

--- 1.2_fstat.c
+++ 1.2_fstat.c
@@ -7,22 +7,6 @@
 
 const ssize_t ctime_r_size = 26;
 
-char* get_file_type(const mode_t sb)
-{
-	switch (sb & S_IFMT)
-        {
-         	case S_IFBLK:  return "block device";            break;
-         	case S_IFCHR:  return "character device";        break;
-         	case S_IFDIR:  return "directory";               break;
-         	case S_IFIFO:  return "FIFO/pipe";               break;
-         	case S_IFLNK:  return "symlink";                 break;
-         	case S_IFREG:  return "regular file";            break;
-         	case S_IFSOCK: return "socket";                  break;
-         	default:       return "unknown?";                break;
-        }
-        
-}
-
 void print_access(const struct stat *sb)
 {
 	printf("access:                   ");
@@ -39,25 +23,35 @@ void print_access(const struct stat *sb)
     	printf("\n");
 }
 
-int main(int argc, char *argv[])
+char* get_file_type(const mode_t sb)
 {
+	switch (sb & S_IFMT)
+        {
+         	case S_IFBLK:  return "block device";            break;
+         	case S_IFCHR:  return "character device";        break;
+         	case S_IFDIR:  return "directory";               break;
+         	case S_IFIFO:  return "FIFO/pipe";               break;
+         	case S_IFLNK:  return "symlink";                 break;
+         	case S_IFREG:  return "regular file";            break;
+         	case S_IFSOCK: return "socket";                  break;
+         	default:       return "unknown?";                break;
+        }
+        
+}
+
+int main()
+{
+	FILE *fp;
 	struct stat sb;
 	char s[ctime_r_size];
 	char *type;
 	struct tm bdt;
 	long int nsec;
 	
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
-		exit(EXIT_FAILURE); 
-	}
-	
-	if(lstat(argv[1], &sb) == -1)
-	{
-		perror("lstat");
-		exit(EXIT_FAILURE);
-	}
+	if ((fp=fopen("test.txt", "rb")) == NULL) 
+		perror("Failed to open file");		
+
+	fstat (fileno (fp), &sb);
 	
 	type = get_file_type(sb.st_mode);
 	
@@ -73,8 +67,8 @@ int main(int argc, char *argv[])
         printf("Preferred I/O block size: %ld bytes\n", (long) sb.st_blksize);
         printf("File size:                %lld bytes\n", (long long) sb.st_size);
         printf("Blocks allocated:         %lld\n", (long long) sb.st_blocks);
-
-	ctime_r(&sb.st_ctime, s);
+        
+        ctime_r(&sb.st_ctime, s);
 	nsec = sb.st_ctim.tv_nsec % 1000000000;
 	localtime_r (&sb.st_ctime, &bdt);
         printf("Last status change:       %s nsec: %ld %s", bdt.tm_zone, nsec, s);
@@ -90,4 +84,8 @@ int main(int argc, char *argv[])
         printf("Last file modification:   %s nsec: %ld %s", bdt.tm_zone, nsec, s);
         
         exit(EXIT_SUCCESS);
+
+	fclose (fp);
+	
+	return 0;
 }
